5e1d83dfa5fe6d341eb918e3518dc28ed307ad83
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-06-28 17:30:17 +0200

    Composer

--- POO/demos/ex-compte-bancaire.php
+++ POO/demos/ex-compte-bancaire.php
@@ -2,7 +2,7 @@
 
 use Openska\Bank\{CompteBancaire,Contact};
 
-require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 try {
     $compteCourant = new CompteBancaire();
